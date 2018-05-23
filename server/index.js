28949ff2c93f98c98893d29707de7acdf8f504eb
Michael Allan <Michael Allan>
2018-05-23 22:16:39 +1000

    Initial graphql schema

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 
+const schema = require('./schema/schema')
+
 const app = express()
 const PORT = 4000
 
-app.use('/graphql', graphqlHTTP({}))
+app.use('/graphql', graphqlHTTP({ schema }))
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
