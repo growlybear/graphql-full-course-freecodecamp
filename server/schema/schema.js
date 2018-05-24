0b52935919633df3b87c449fd4075a9abc420a41
Michael Allan <Michael Allan>
2018-05-24 10:34:12 +1000

    Add book > author relationship

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -11,9 +11,9 @@ const _ = require('lodash')
 
 // dummy data
 const books = [
-  { id: '1', name: 'Name of the Wind', genre: 'Fantasy' },
-  { id: '2', name: 'The Final Empire', genre: 'Fantasy' },
-  { id: '3', name: 'The Long Earth', genre: 'Sci-Fi' },
+  { id: '1', name: 'Name of the Wind', genre: 'Fantasy', authorId: '1' },
+  { id: '2', name: 'The Final Empire', genre: 'Fantasy', authorId: '2' },
+  { id: '3', name: 'The Long Earth', genre: 'Sci-Fi', authorId: '3' },
 ]
 
 const authors = [
@@ -28,6 +28,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve: (parent, args) => {
+        console.log(parent)
+        return _.find(authors, { id: parent.authorId })
+      }
+    }
   })
 })
 
