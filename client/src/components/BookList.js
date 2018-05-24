e98132b305c465a18441689c6589a182ab222e83
Michael Allan <Michael Allan>
2018-05-24 22:22:39 +1000

    Fetch book data from GraphQL - W00t

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { gql } from 'apollo-boost'
+import { graphql } from 'react-apollo'
+
+const getBooksQuery = gql`
+  {
+    books {
+      name,
+      genre,
+      id
+    }
+  }
+`
 
 class BookList extends Component {
+  displayBooks () {
+    let { data } = this.props
+    return data.loading
+      ? <li>Fetching book data...</li>
+      : data.books.map(book => <li>{ book.name }</li>)
+  }
   render () {
+    console.log(this.props)
     return (
-      <div>
-        <li>Book name</li>
-      </div>
+      <ul>
+        { this.displayBooks() }
+      </ul>
     )
   }
 }
 
-export default BookList
+export default graphql(getBooksQuery)(BookList)
